3d2d83ec1962e1d8076aa1670b77eac60fb61938
Sergio Rodriguez <sergio.rodriguez@bsc.es>
2017-11-13 20:19:10 +0100

    removed satsolvers from repo

--- minisat/core/depend.mk
+++
@@ -1,9 +0,0 @@
-/home/srodrig1/git/CellRouter/minisat/core/Solver.o /home/srodrig1/git/CellRouter/minisat/core/Solver.or /home/srodrig1/git/CellRouter/minisat/core/Solver.od /home/srodrig1/git/CellRouter/minisat/core/Solver.op: /home/srodrig1/git/CellRouter/minisat/core/Solver.cc \
- ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
- ../core/Solver.h ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h \
- ../utils/ParseUtils.h ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h
-/home/srodrig1/git/CellRouter/minisat/core/Main.o /home/srodrig1/git/CellRouter/minisat/core/Main.or /home/srodrig1/git/CellRouter/minisat/core/Main.od /home/srodrig1/git/CellRouter/minisat/core/Main.op: /home/srodrig1/git/CellRouter/minisat/core/Main.cc \
- ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
- ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
- ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
- ../core/Solver.h ../mtl/Heap.h
